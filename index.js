5463702fc8321ffbcb228891cb7cad3294410874
lema samuel <lemasam10@gmail.com>
2024-04-01 19:32:00 +0300

    arrays done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+     let found = record.find((element) => element.result === "W");
+      if (found) {
+        return found.year;
+      } else {
+        return undefined;
+      }
+    }
+
